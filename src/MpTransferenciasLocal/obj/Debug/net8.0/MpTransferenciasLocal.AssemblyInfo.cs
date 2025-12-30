8a2826af9b181421d8238b61d8849a4d3a82fde4
gonchu84 <31147239+gonchu84@users.noreply.github.com>
2025-12-29 22:01:35 -0300

    Fix controller path + map controllers

--- src/MpTransferenciasLocal/obj/Debug/net8.0/MpTransferenciasLocal.AssemblyInfo.cs
+++ src/MpTransferenciasLocal/obj/Debug/net8.0/MpTransferenciasLocal.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MpTransferenciasLocal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+525bbce10aa6f327b80feaab321ae1e582d13f80")]
 [assembly: System.Reflection.AssemblyProductAttribute("MpTransferenciasLocal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MpTransferenciasLocal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
